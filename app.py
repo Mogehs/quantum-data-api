96bbe112ab53a7fabfe408ebfd918fbe7eca070d
Developer Shoaib <shoaibbinallahbakhsh@gmail.com>
2026-02-05 19:43:27 +0500

    updated the host

--- app.py
+++ app.py
@@ -591,4 +591,4 @@ async def get_mock_data(asset: str = "equities", symbol: str = "AAPL", tf: str =
 if __name__ == "__main__":
     import os
     port = int(os.environ.get("PORT", 10000))
-    uvicorn.run("app:app", host="127.0.0.1", port=port, reload=True)
+    uvicorn.run("app:app", host="0.0.0.0", port=port, reload=True)
